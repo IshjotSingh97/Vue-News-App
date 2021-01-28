8b803e93f22905a5a22695edc610bd740b57fda3
Ishjot Singh <ishjotsinghahluwalia@gmail.com>
2021-01-28 12:53:11 +0530

    dark mode added

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const API = 'https://saurav.tech/NewsAPI/top-headlines/category/option/in.json'
+let API = 'https://saurav.tech/NewsAPI/top-headlines/category/option/in.json'
 const appComponent = {
 	data(){
 		return {
-		usercategory : '',
+		usercategory : null,
 		darkmode : false,
-		categories : ["business","entertainment","general","health","science","sports","technology"]
+		categories : ["business","entertainment","general","health","science","sports","technology"],
+		articles : []
 		}
 	},
 	methods : {
-
+		async fetchNews(){
+			let query = API.replace("option",this.usercategory)
+			console.log(query)
+			await axios
+			.get(query)
+			.then((response)=>{
+				console.log(response.data)
+				this.articles = response.data.articles
+			})
+			.catch((error)=>{
+				console.log(error)
+			})
+		}
 	}
 }
 const myapp = Vue.createApp(appComponent).mount('#myapp')
